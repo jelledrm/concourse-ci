dc0ef47df201321a0a06e6f26c5916f8c7c0c960
Jelldrm <Jelle2510@gmail.com>
2017-04-10 21:36:20 +0200

    add docker-compose.yml

--- generatekeys.sh
+++ generatekeys.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 mkdir -p keys/web keys/worker/worker_key
-ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N 'qdsdqsd'
-ssh-keygen -t rsa -f ./keys/web/session_signing_key -N 'sqdsqdqsd'
-ssh-keygen -t rsa -f ./keys/worker/worker_key -N 'qsdqsdsqsq'
+ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N ""
+ssh-keygen -t rsa -f ./keys/web/session_signing_key -N ""
+ssh-keygen -t rsa -f ./keys/worker/worker_key  -N ""
 cp ./keys/worker/worker_key.pub ./keys/web/authorized_worker_keys
 cp ./keys/web/tsa_host_key.pub ./keys/worker
